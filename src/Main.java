518bff583344071992dcdb329d7951fa71169a18
khakimov firdavs <killervani2281337@gmail.com>
2023-04-23 19:35:41 +0600

    4 th commit

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // Примитивы - int, float, double, boolean
-        // Ссылочные - String, Integer, Double
-        /*
-        Scanner scanner = new Scanner(System.in);
+    /*public static void main(String[] args) {
+    int i = 1;
+        do {
+            while (i = 1; i <= 100; i++) {
+                System.out.println(i);
 
-        int number = scanner.nextInt();
+     */
 
-        int i = 1;
-
-        while (i*i<number) {
-            System.out.println(i*i);
-            i += 1;
         }
-        */
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        int i = 2;
-        while (number % i != 0){
-            i++;
-        }
-        System.out.println(i);
-
-        System.out.println("hello");
     }
 }
